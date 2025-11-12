3cb5cdc497ebb82d7a5fbc0571fc6e13ad3e759c
ChoeTaeWoon <xodnswh123@gmail.com>
2025-11-12 23:04:04 +0900

    하이

---
+++ src/main/java/com/fooddet/FooddetApiApplication.java
@@ -0,0 +1,11 @@
+package com.fooddet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddetApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FooddetApiApplication .class, args);
+	}
+}
